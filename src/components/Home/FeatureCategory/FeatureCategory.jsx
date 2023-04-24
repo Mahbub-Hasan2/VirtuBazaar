77a3ca84651f874c712af632efbffcde1f86f052
ND Morsalin <morsalin056@gmail.com>
2023-04-24 09:15:08 -0700

    Feature data is ready

---
+++ src/components/Home/FeatureCategory/FeatureCategory.jsx
@@ -0,0 +1,64 @@
+//TODO this is heard coded data for feature category it will be dynamic later
+const data ={
+    "super":[
+        "Beauty & Fragrances",
+        "Construction & Hardware",
+        "Home & Furniture",
+        "Health & Personal Care",
+        "Sports & Fitness"
+        ],
+        "main":[
+        {
+        "name": "Strawberry",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?strawberry"
+        },
+        {
+        "name": "Fitness & Training",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?fitness"
+        },
+        {
+        "name": "Makeup",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?makeup"
+        },
+        {
+        "name": "Mobile phone & Tablets",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?technology"
+        },
+        {
+        "name": "Cake & Milk",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?cake"
+        },
+        {
+        "name": "Coffee & Tea",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?coffee,tea"
+        },
+        {
+        "name": "Vegetable",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?vegetable"
+        },
+        {
+        "name": "Books",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?books"
+        },
+        {
+        "name": "Arts & crafts",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?arts,crafts"
+        },
+        {
+        "name": "Snack",
+        "categoryImage": "https://source.unsplash.com/random/400x400/?snack"
+        }
+        ]
+        
+}
+// ! this is heard coded data for feature category it will be dynamic later
+
+const FeatureCategory = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FeatureCategory;
