d1275b6b87dea3a6620b3bd2db74e28ea82e9c85
ND Morsalin <morsalin056@gmail.com>
2023-04-26 00:17:24 -0700

    feature category section is done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import FeatureCategory from "../../components/Home/FeatureCategory/FeatureCategory";
 import HeroSection from "../../components/Home/HeroSection/HeroSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <FeatureCategory />
     </div>
   );
 };
